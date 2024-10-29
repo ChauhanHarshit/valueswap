d30acc3e060f8656dca83d0350be09c3b7c7ba52
Harshit Chauhan <kcn9596@gmail.com>
2024-10-30 00:38:26 +0530

    fixed bugs for params

--- scripts/helper_scripts/local_transfer.sh
+++ scripts/helper_scripts/local_transfer.sh
@@ -4,4 +4,7 @@
 
 # dfx canister call LP_ledger_canister icrc1_balance_of "(record {owner=principal\"um2uc-3w7jg-5lycg-pmjio-is7ms-jjzwo-kvwfa-xdeus-ur2tx-4sbzd-cae\"; subaccount=null})"
 
-dfx canister call ckbtc_ledger icrc1_transfer ' (record {to=record {owner = principal "by6od-j4aaa-aaaaa-qaadq-cai"; subaccount=null}; fee=null; memo=null; from_subaccount=null; created_at_time=null; amount=50000000})'
+# dfx canister call ckbtc_ledger icrc1_transfer ' (record {to=record {owner = principal "rgtib-ktq4g-rjiya-aishg-mv2om-ey54m-jhyut-2tuaf-tvjyz-ml54r-lae"; subaccount=null}; fee=null; memo=null; from_subaccount=null; created_at_time=null; amount=5000000000})'
+
+
+dfx canister call LP_ledger_canister icrc1_transfer ' (record {to=record {owner = principal "rby6od-j4aaa-aaaaa-qaadq-cai"; subaccount=null}; fee=null; memo=null; from_subaccount=null; created_at_time=null; amount=500000000})'
